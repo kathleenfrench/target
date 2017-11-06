792bbda839a6a54d836264861b6f2f29cbdb5391
Kathleen French <kfrench09@gmail.com>
2017-11-05 21:36:44 -0500

    add easing animation to bricks on kill

--- game.js
+++ game.js
@@ -94,7 +94,12 @@ function initBricks(){
 
 function ballHitBrick(ball, brick){
   ball.animations.play('wobble');
-  brick.kill();
+  var killTween = game.add.tween(brick.scale);
+  killTween.to({x: 0, y: 0}, 200, Phaser.Easing.Linear.None);
+  killTween.onComplete.addOnce(function(){
+    brick.kill();
+  }, this);
+  killTween.start();
   score += 10;
   scoreText.setText('Score: ' + score);
 
