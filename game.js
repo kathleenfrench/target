fc4c95e9ee88fdb6e27e5b08bf0d2e188231ac78
Kathleen French <kfrench09@gmail.com>
2017-11-05 21:04:29 -0500

    add paddle

--- game.js
+++ game.js
@@ -5,9 +5,11 @@ var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
 });
 
 var ball;
+var paddle;
 
 function preload(){
   game.load.image('ball', 'assets/ball.png');
+  game.load.image('paddle', 'assets/paddle.png');
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   game.scale.pageAlignHorizontally = true;
   game.scale.pageAlignVertically = true;
@@ -17,6 +19,8 @@ function preload(){
 function create(){
   game.physics.startSystem(Phaser.Physics.ARCADE);
   ball = game.add.sprite(50, 50, 'ball');
+  paddle = game.add.sprite(game.world.width * 0.5, game.world.height - 5, 'paddle');
+  paddle.anchor.set(0.5, 1);
   game.physics.enable(ball, Phaser.Physics.ARCADE);
   ball.body.velocity.set(150, 150);
   ball.body.collideWorldBounds = true;
